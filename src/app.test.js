5cc491745498adac8961a621fdaf025f9258064c
vospader13 <justenmcdonald@gmail.com>
2020-05-06 17:48:47 -0600

    got some functioning tests.

--- src/app.test.js
+++ src/app.test.js
@@ -1,8 +1,36 @@
+  
 import React from "react";
-import { render } from '@testing-library/react';
+import Dropdown from "./App";
+import { render, fireEvent, waitforElement, wait, screen } from "@testing-library/react";
 import App from "./App";
-
 
 test("renders App without crashing", () => {
     render(<App />);
   });
+
+test("show list of seasons when clicked on", () => {
+    const testSeason = 'test Season'
+    render(<Dropdown>{testSeason}</Dropdown>)
+
+    expect(screen.queryByText(testSeason)).toBeNull()
+})
+
+test("Making sure getting the text while fetching the data", async () => {
+  const { getByText } = render(<App />);
+
+  const text = getByText(/Fetching data.../i);
+  expect(text).toBeInTheDocument();
+});
+
+
+
+test('fetch makes api call to proper url', async () => {
+  const axios = {
+      get: jest.fn(() => Promise.resolve({data: {episodes: 'Season'}})),
+  }
+  const url = 'https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
+  render(<App url={url}/>)
+  expect(axios.get).toHaveBeenCalledTimes(0)
+  
+})
+
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Episodes from './Episodes';
+import React from "react";
+import {render, wait } from "@testing-library/react";
+import Episodes from "./Episodes";
+
+const episodes = [
+    {
+        "id":553946,
+        "url":"http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+        "name":"Chapter One: The Vanishing of Will Byers",
+        "season":1,
+        "number":1,
+        "airdate":"2016-07-15",
+        "airtime":"",
+        "airstamp":"2016-07-15T12:00:00+00:00",
+        "runtime":60,
+    },
+    
+    {
+        "id":578663,
+        "url":"http://www.tvmaze.com/episodes/578663/stranger-things-1x02-chapter-two-the-weirdo-on-maple-street",
+        "name":"Chapter Two: The Weirdo on Maple Street",
+        "season":1,
+        "number":2,
+        "airdate":"2016-07-15",
+        "airtime":"",
+        "airstamp":"2016-07-15T12:00:00+00:00",
+        "runtime":60,
+    },
+]
 
 
-test("", () => {
-    const { queryAllByTestId, rerender } = render(
-        <Episodes error="" episodes={[]} />
+
+
+test("Episodes renders correctly", async() => {
+    const { queryAllByText } = render(
+        <Episodes episodes={episodes} />
     );
-    expect(queryAllByTestId("episodes")).toStrictEqual([]);
-    expect(queryAllByTestId("episodes")).toHaveLength(0);
+
+    expect(queryAllByText(/episode/i)).toHaveLength(2);
 });
